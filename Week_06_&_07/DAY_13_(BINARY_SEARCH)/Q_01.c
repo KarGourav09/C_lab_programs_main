ffc01024747ebb9af322ed88a0b7ba51819f37ee
Gourav Kar <Kar541049@gmail.com>
2025-10-09 14:06:56 +0530

    modified:   Week_06_&_07/DAY_13_(BINARY_SEARCH)/Q_01.c 	deleted:    Week_06_&_07/DAY_13_(BINARY_SEARCH)/Q_01.exe

--- Week_06_&_07/DAY_13_(BINARY_SEARCH)/Q_01.c
+++ Week_06_&_07/DAY_13_(BINARY_SEARCH)/Q_01.c
@@ -1,12 +1,14 @@
-/*Initialse and Display a 2-d array*/
+/*WAP for a two dimensional array to store and
+display the elements. (Store temperature of two cities for a week and display
+it)*/
 
 #include <stdio.h>
 
 int main() {
     int a,b,n;
-    printf("Enter the number of coulmn in the array: ");
+    printf("Enter number of cities: ");
     scanf("%d", &a);
-    printf("Enter the number of row in the array: ");
+    printf("Enter number of days: ");
     scanf("%d", &b);
 
     n = a*b;
@@ -17,17 +19,17 @@ int main() {
     {
         for(int j = 0; j < b; j++)
         {
-        printf("Enter the number[%d][%d]: ", i,j);
+        printf("Cities[%d] Days[%d]: ", i+1,j+1);
         scanf("%d", &arr[i][j]);
         }
     }
 
-    printf("Array contents are: ");
+    printf("2D Array contents are: ");
     for(int i = 0; i < a; i++)
     {
         for(int j = 0; j < b; j++)
         {
-         printf(" %d\n",arr[i][j]);
+         printf("Cities[%d] Days[%d]: %d\n",i+1,j+1, arr[i][j]);
         }
         printf("\n");
     }
